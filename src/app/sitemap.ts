f45e328dd0953f7303e214dfac6a978fa0a1f64c
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2026-04-09 20:49:07 -0700

    fix: mark the sitemap as dynamic so we do not try to access mongodb_url at build env

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { type MetadataRoute } from "next";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL ?? "https://cassiusreynolds.dev";
